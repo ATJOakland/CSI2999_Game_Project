df03e2ac6c26d6a1c7453f4f8d87640e7f7764f5
mwilson2024 <145935125+mwilson2024@users.noreply.github.com>
2023-09-24 16:29:18 -0400

    Add files via upload

---
+++ CSI2999/src/csi2999/CSI2999.java
@@ -0,0 +1,13 @@
+package csi2999;
+
+import javax.swing.SwingUtilities;
+public class CSI2999 {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable(){
+                public void run(){
+                new MainMenu();
+                }
+        });
+    }
+}
+
