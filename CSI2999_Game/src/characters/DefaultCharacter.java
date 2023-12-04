035ed3cd2fedcd7bae0b0cb3ec70814b3dc050cd
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2023-12-03 19:50:50 -0500

    Update DefaultCharacter.java

--- CSI2999_Game/src/characters/DefaultCharacter.java
+++ CSI2999_Game/src/characters/DefaultCharacter.java
@@ -5,4 +5,10 @@ public class DefaultCharacter {
 	public int posX, posY, tempPosX, tempPosY;
 	public int characterSpeed;
 	public boolean isInBattle = false; // Checks if in Battle or not.
+	public BufferedImage up, down, left, right, up1, up2, down1, down2, left1, left2, right1, right2;
+	public String direction = "down";
+	public Rectangle solidArea;
+	public boolean colOn = false;
+	public int spriteCounter = 0;
+	public int spriteNum = 0;
 }
