09a552f6c9b219b60b81fbdfd30fde38d50690fa
ATJ <145053177+ATJOakland@users.noreply.github.com>
2023-12-03 20:21:20 -0500

    Add files via upload

---
+++ CSI2999_Game/src/characters/DefaultCharacter.java
@@ -0,0 +1,8 @@
+package characters;
+
+public class DefaultCharacter {
+	// Default variables for ALL types of characters (player and enemies and even NPCs)
+	public int posX, posY, tempPosX, tempPosY;
+	public int characterSpeed;
+	public boolean isInBattle = false; // Checks if in Battle or not.
+}
