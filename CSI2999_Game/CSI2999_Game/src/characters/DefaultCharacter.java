28e6ec6de8f15389b24fba98dcdd89e39385142e
ATJ <145053177+ATJOakland@users.noreply.github.com>
2023-12-03 20:26:11 -0500

    Delete CSI2999_Game/CSI2999_Game directory

--- CSI2999_Game/CSI2999_Game/src/characters/DefaultCharacter.java
+++
@@ -1,8 +0,0 @@
-package characters;
-
-public class DefaultCharacter {
-	// Default variables for ALL types of characters (player and enemies and even NPCs)
-	public int posX, posY, tempPosX, tempPosY;
-	public int characterSpeed;
-	public boolean isInBattle = false; // Checks if in Battle or not.
-}
